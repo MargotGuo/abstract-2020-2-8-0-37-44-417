bb932a89b742f9140bd5b4c182aa915db27e6d7c
MargotGuo <gyqgdn@126.com>
2020-02-11 22:16:21 +0800

    [Guo Yuanqi]{fix}: for declarations

--- abstract/src/main/java/com/Demo.java
+++ abstract/src/main/java/com/Demo.java
@@ -3,9 +3,9 @@ package com;
 public class Demo {
 
   public static void main(String[] args) {
-    Cock cock = new Cock();
-    Hen hen = new Hen();
-    ChildChicken childChicken = new ChildChicken();
+    Chicken cock = new Cock();
+    Chicken hen = new Hen();
+    Chicken childChicken = new ChildChicken();
 
     System.out.format("100元可以买%s %d只\n", cock.getType(), (int)(100 / cock.getPrice()));
     System.out.format("100元可以买%s %d只\n", hen.getType(), (int)(100 / hen.getPrice()));
